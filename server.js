31e8d49d38214db1a54b4bd2675690c69fd61bc1
Jericho Muhi <138776450+muhijericho@users.noreply.github.com>
2025-08-07 19:25:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get(`/`, (req, res) => {
             <head>
             </head>
             <body >
-                <div style="text-align: center; width: 45%; height: 50%; background-color: #ddd9d9ff; margin-left: 100px;">
-                    <h1 style="color: red;">Server Under Maintenance</h1>
+                <div style="text-align: center; width: 45%; height: 50%; background-color: blue; margin-left: 100px;">
+                    <h1 style="color: White;">Server Under Maintenance</h1>
                     <p>We are currently performing scheduled maintenance. We apologize for any inconvenience this may cause.</p>
                     <p>Please check back later.</p>
                     <p>Thank you for your patience!</p>
@@ -22,5 +22,6 @@ app.get(`/`, (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
 
 
